7d8f1489bf6282ec40617e68563cea78dd17674a
Kevin See <kevinsee@kevins-mbp-2.raleigh.ibm.com>
2019-10-07 14:25:43 -0400

    update docker push script

--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -3,32 +3,50 @@
 IMAGE=$1
 
 if [ -z "$IMAGE" ]; then
-    echo "Usage: $0 IMAGE"
-    exit 1
+  echo "Usage: $0 IMAGE"
+  exit 1
 fi
 
+if [ -z "$DOCKER_REGISTRY_PASSWORD" ]; then
+  echo "You MUST set the DOCKER_REGISTRY_PASSWORD environment variable"
+  exit 1
+fi
+
+if [ -z "$DOCKER_REGISTRY_USER" ]; then
+  echo "You MUST set the DOCKER_REGISTRY_USER environment variable"
+  exit 1
+fi
+
+REG_IMAGE="${IMAGE%%:*}"
+IMAGE_NAME="${REG_IMAGE##*/}"
 IMAGE_TAG="${IMAGE##*:}"
 if [ "$IMAGE" = "$IMAGE_TAG" ]; then
-    IMAGE_TAG=latest
+  IMAGE_TAG=latest
 fi
-
 if [ "$IMAGE_TAG" = "latest" ]; then
-    IMAGE_OVERRIDE=true
+  IMAGE_OVERRIDE=true
 else
-    if [ -z "$IMAGE_OVERRIDE" ] || [ "$IMAGE_OVERRIDE" = "default" ]; then
-        IMAGE_OVERRIDE=false
-    fi
+  if [ -z "$IMAGE_OVERRIDE" ] || [ "$IMAGE_OVERRIDE" = "default" ]; then
+    IMAGE_OVERRIDE=false
+  fi
 fi
 
 if docker pull $IMAGE >/dev/null 2>&1; then
-    if [ "$IMAGE_OVERRIDE" = "true" ]; then
-        echo "Updating existing $IMAGE image."
-        docker push "$IMAGE"
-    else
-        echo "$IMAGE image already exists."
+if [[ ${REGISTRY} == *"artifactory"* ]]; then
+    TAG_CHECK=$(curl -su "${DOCKER_REGISTRY_USER}:${DOCKER_REGISTRY_PASSWORD}" "https://${REGISTRY}/artifactory/api/docker/hyc-icap-open-site-images-docker-local/v2/icpa/${IMAGE_NAME}/tags/list" | jq ".tags[] | select(. == \"${IMAGE_TAG}\")")
+elif [[ ${REGISTRY} == *"stg.icr.io"* ]]; then
+    ibmcloud login --apikey "${DOCKER_REGISTRY_PASSWORD}" -a test.cloud.ibm.com -r us-south
+    if [ "$?" -ne 0 ]; then
+        echo "Failed to login to ibmcloud"
         exit 1
     fi
+    TAG_CHECK=$(ibmcloud cr image-list --format "{{ if and (eq .Repository \"stg.icr.io/cp/icpa/${IMAGE_NAME}\") (eq .Tag \"${IMAGE_TAG}\") }}{{ .Tag }}{{ end }}")
 else
-    echo "Uploading new $IMAGE image."
-    docker push "$IMAGE"
+    echo "Invalid registry ${REGISTRY}"
+    exit 1
 fi
+
+if [ ! -z "$TAG_CHECK" ]; then
+  if [ "$IMAGE_OVERRIDE" = "true" ]; then
+    echo "Updating existing $IMAGE image."
+    docker push "$IMAGE"
