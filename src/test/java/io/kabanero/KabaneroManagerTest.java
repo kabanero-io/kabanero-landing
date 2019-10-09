b3a301b3bfd8a60c6e5cfb0b93dd1f05735b15bd
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-10-09 14:51:25 -0400

    updating test

--- src/test/java/io/kabanero/KabaneroManagerTest.java
+++ src/test/java/io/kabanero/KabaneroManagerTest.java
@@ -4,28 +4,18 @@ import io.kabanero.*;
 import java.util.Arrays;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-public class KabaneroInstanceTest {
+public class KabaneroManagerTest {
   KabaneroRepository kabaneroRepository = new KabaneroRepository("User repositroy name", " https://github.com/appsody/stacks/releases/latest/download/incubator-index.yaml", true);
   KabaneroCollection kabaneroCollection = new KabaneroCollection("nodejs", "2.0");
   KabaneroInstance kabaneroInstance = new KabaneroInstance("kabUser", "kabanero-instance", "10/03/2019", Arrays.asList(kabaneroRepository), "MyCluster", Arrays.asList(kabaneroCollection), "cliURL");
 
   @Test
-  public void kabaneroInstanceNameTest() {
-    assertEquals(kabaneroInstance.instanceName, "kabanero-instance");
-  }
-
-  @Test
-  public void kabaneroInstanceNameDetail() {
-    assertEquals(kabaneroInstance.getDetails(), kabaneroInstance.details);
-  }
-
-  @Test
   public void kabaneroInstanceEndpointTest() {
-    KabaneroManager.getKabaneroManagerInstance().addInstance(kabaneroInstance);
-    InstanceEndpoints instanceEndpoints = new InstanceEndpoints();
-    assertEquals(instanceEndpoints.getAInstance("kabanero-instance").getStatus(), 200);
+    KabaneroManager kMan = KabaneroManager.getKabaneroManagerInstance(); 
+    kMan.addInstance(kabaneroInstance);
+    assertNotNull(kMan.getKabaneroInstance(kabaneroInstance.getInstanceName()));
   }
 
 }
