f016deb924c3e13416b37aa3ce4cf4030dedcc4c
Andrew Lohr <alohr51@users.noreply.github.com>
2019-12-02 12:47:52 -0500

    add -p option to mkdir for carbon icons dir (#88)

--- scripts/build_jekyll_maven.sh
+++ scripts/build_jekyll_maven.sh
@@ -11,7 +11,7 @@ ruby -v
 npm install
 # Move carbon icons so jekyll will include them
 CARBON_ICONS_DIR="$CONTENT_DIR/img/carbon-icons/"
-mkdir $CARBON_ICONS_DIR
+mkdir -p $CARBON_ICONS_DIR
 mv ./node_modules/carbon-icons/dist/carbon-icons.svg ./node_modules/carbon-icons/dist/svg/* "$CONTENT_DIR/img/carbon-icons/"
 
 ./scripts/build_gem_dependencies.sh
