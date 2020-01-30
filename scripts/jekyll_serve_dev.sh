42d55660443ccded537b7f7f68c4c1c8b326d82c
Andrew Lohr <ajlohr@us.ibm.com>
2020-01-30 14:51:40 -0500

    update liberty maven plugin to use dev mode

--- scripts/jekyll_serve_dev.sh
+++ scripts/jekyll_serve_dev.sh
@@ -4,9 +4,9 @@ CONTENT_DIR="src/main/content"
 
 npm install
 
-# Move carbon icons so jekyll will include them
+# Copy carbon icons so jekyll will include them
 CARBON_ICONS_DIR="$CONTENT_DIR/img/carbon-icons/"
 mkdir $CARBON_ICONS_DIR
-mv ./node_modules/carbon-icons/dist/carbon-icons.svg ./node_modules/carbon-icons/dist/svg "$CONTENT_DIR/img/carbon-icons/"
+cp -t "$CONTENT_DIR/img/carbon-icons/" ./node_modules/carbon-icons/dist/carbon-icons.svg ./node_modules/carbon-icons/dist/svg
 
 bundle exec jekyll s --host 0.0.0.0 --source src/main/content --config src/main/content/_config.yml,src/main/content/_dev_config.yml --drafts
