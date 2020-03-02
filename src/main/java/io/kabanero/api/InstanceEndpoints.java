f4b0e8fc28fda158085fc7752fb10339f35db88d
kidus.sw@gmail.com <kidus.sw@gmail.com>
2020-03-02 11:48:10 -0500

    API endpoint that lists all members

--- src/main/java/io/kabanero/api/InstanceEndpoints.java
+++ src/main/java/io/kabanero/api/InstanceEndpoints.java
@@ -20,6 +20,7 @@ package io.kabanero.api;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.ApplicationPath;
@@ -31,14 +32,24 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.http.client.ClientProtocolException;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.ibm.websphere.security.social.UserProfile;
+import com.ibm.websphere.security.social.UserProfileManager;
 
-import io.website.ResponseMessage;
+import org.apache.http.client.ClientProtocolException;
+import org.eclipse.egit.github.core.Team;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.TeamService;
+
 import io.kabanero.v1alpha2.models.Kabanero;
 import io.kabanero.v1alpha2.models.KabaneroList;
 import io.kabanero.v1alpha2.models.StackList;
-import io.kubernetes.client.ApiException;
 import io.kubernetes.KabaneroClient;
+import io.kubernetes.client.ApiException;
+import io.website.ResponseMessage;
 
 @ApplicationPath("api")
 @Path("/kabanero")
@@ -72,11 +83,50 @@ public class InstanceEndpoints extends Application {
     @GET
     @Path("/{instanceName}/stacks")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response listStacks(@PathParam("instanceName") String instanceName) throws ClientProtocolException, IOException, ApiException, GeneralSecurityException {
+    public Response listStacks(@PathParam("instanceName") String instanceName)
+            throws ClientProtocolException, IOException, ApiException, GeneralSecurityException {
         StackList stacks = KabaneroClient.getStacks(instanceName);
-        if(stacks == null){
-            return Response.status(404).entity(new ResponseMessage("Stacks do not exist for instance: " + instanceName)).build();
+        if (stacks == null) {
+            return Response.status(404).entity(new ResponseMessage("Stacks do not exist for instance: " + instanceName))
+                    .build();
         }
         return Response.ok(stacks).build();
+    }
+
+    @GET
+    @Path("{instanceName}/team/{teamName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response isAdmin(@PathParam("instanceName") String instanceName, @PathParam("teamName") String teamName) throws IOException, ApiException, GeneralSecurityException {
+        UserProfile userProfile = UserProfileManager.getUserProfile();
+        String token = userProfile.getAccessToken();
+        GitHubClient client = new GitHubClient();
+        client.setOAuth2Token(token);
+
+        Kabanero instance = KabaneroClient.getAnInstance(instanceName);
+        if (instance == null) {
+            return Response.status(404).entity(new ResponseMessage(instanceName + " not found")).build();
+        }
+
+        JsonObject body = new JsonObject();
+        String instanceGithubOrg = instance.getSpec().getGithub().getOrganization();
+        TeamService teamService = new TeamService(client);
+
+        for (Team orgTeam : teamService.getTeams(instanceGithubOrg)) {
+            if (teamName.equals(orgTeam.getName())) {
+
+                List<User> kabaneroTeamMembers = teamService.getMembers(orgTeam.getId());
+                JsonArray jsonArray = new Gson().toJsonTree(kabaneroTeamMembers).getAsJsonArray();
+                body.add("members", jsonArray);
+
+                if (kabaneroTeamMembers.size() == 0) {
+                    return Response.status(404)
+                            .entity(new ResponseMessage(teamName + " currently does not have any members")).build();
+                }
+                
+            } else {
+                return Response.status(404).entity(new ResponseMessage(teamName + " team not found")).build();
+            }
+        }
+        return Response.ok(body).build();
     }
 }
